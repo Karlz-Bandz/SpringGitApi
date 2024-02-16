5c143ce2295b3e2f1935609cf9aa0fb1dfc7adb9
Melak <km250542@ncratleos.com>
2024-02-16 20:02:00 +0100

    Initial commit

---
+++ src/test/java/com/gitapi/gitapi/GitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitapi.gitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
